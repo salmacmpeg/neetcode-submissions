163b776315694414bb34bd47638760c300250be5
Salma Abd El Monem <40614417+salmacmpeg@users.noreply.github.com>
2026-07-31 17:20:37 +0300

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums)<1:
+            return 0
+        hashes = {}
+        for n in nums:
+            hashes[n]=1
+
+        longest = 1
+        for n in nums:
+            if (n-1) not in hashes:
+                hashes[n]=1 #possible start
+                curr =n+1
+                while curr in hashes:
+                    hashes[n]+=1
+                    curr+=1
+                if hashes[n] > longest:
+                    longest =hashes[n]
+        return longest
