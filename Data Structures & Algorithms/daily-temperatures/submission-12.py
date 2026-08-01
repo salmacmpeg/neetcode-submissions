b6a778cb8391e463320e0b0fa9cca5a3e89d2ed4
Salma Abd El Monem <40614417+salmacmpeg@users.noreply.github.com>
2026-08-01 17:05:11 +0300

    Add: daily-temperatures - submission-12

---
+++ Data Structures & Algorithms/daily-temperatures/submission-12.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        s =[]
+        for i , t in enumerate(temperatures):
+            while len(s)>0 and t>temperatures[s[-1]]:
+                res[s[-1]]= i - s[-1]
+                s.pop()
+            s.append(i)
+
+        return res
