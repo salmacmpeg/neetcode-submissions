3118064abe230e7f193856dfc6ac8cb5dc07ba58
Salma Abd El Monem <40614417+salmacmpeg@users.noreply.github.com>
2026-08-15 12:29:00 +0300

    Add: task-scheduling - submission-45

---
+++ Data Structures & Algorithms/task-scheduling/submission-45.py
@@ -0,0 +1,32 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        hashy = {}
+        heap = []
+        for elem in tasks:
+            hashy[elem] = 1 + hashy.get(elem, 0)
+        
+        for elem, freq in hashy.items():
+            heapq.heappush(heap, (-freq,0))
+        del hashy
+
+        
+        i =1
+        queue = deque()
+        t =qt =0
+        while len(heap)+len(queue)!= 0:
+            if len(heap)>0:
+                freq,t = heap[0]
+                if t <=i: #process the queue
+                    freq,t = heapq.heappop(heap)
+                    freq = -freq
+                    if freq>1:
+                        queue.append(((freq-1),i+n))
+            if len(queue)>0:
+                qfreq,qt = queue[0]
+                if qt <=i:
+                    qfreq,qt = queue.popleft()
+                    heapq.heappush(heap, (-qfreq,qt))
+
+            i+=1
+  
+        return i-1
